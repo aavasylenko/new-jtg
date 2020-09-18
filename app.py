4d7a15f423e5c23f07200c9755ff88a94488881b
aavasylenko <aavasylenko@hotmail.com>
2020-09-18 18:50:08 +0000

    Added new requirements and change debug to false for prod version

--- app.py
+++ app.py
@@ -162,4 +162,4 @@ def edit_product(product_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
